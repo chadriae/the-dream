4c8dc21d8ed5e3c43cf8673fcb35df10c7c793f6
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-01-06 19:40:18 +0100

    Add styling

--- README.md
+++ README.md
@@ -1,45 +1,25 @@
-# The dream 🍹
+# The Dream : Currency converter 💸
 
-- Repository: `the-dream`
-- Type of Challenge: `Learning`
-- Duration: `1 day`
-- Team challenge : `solo`
+## 👩‍💻 Learning objectives
 
-## The Mission
+- Figure out what a form submit actually means / does
+- How to get the form data after the submit
+- Figure out a way to provide data in the HTML
 
-A lockdown forbids a lot of things, but your imagination has all the freedom it needs.
-What if we were thirsty from climbing the table mountain in South-Africa right now, or sunbathing a nice Hawaiian beach and in dire need of a cocktail?
-One thing is for sure - we'd need to calculate how many Euros that way-too-expensive drink will be 💸
+## 🎖 Mission
 
-### Specifications
+### Must-have features
 
-#### Must-have features
-
-- Pick your next travel destination and check the exchange rate: local currency / Euros.
+- Check the exchange rate: local currency / Euros.
 - Make a small webpage where you can enter the local rate (use a form) and it'll tell you the price in Euros.
 - Use a form that submits to PHP (no JS allowed / no preventDefault)
 
-#### Nice-to-have features
+### Nice-to-have features
 
 - Allow the user to choose between different currencies
 - Provide a button to switch the from / to valuta types (So Euro -> Dollar becomes Dollar --> Euro)
 
-## Need some help?
+## 🎤 Tools
 
-The [PHP basics](https://github.com/becodeorg/gnt-verou-1-26/blob/master/3.The-Mountain/0.Getting-started-with-PHP/PHP-basics.md) contains some helpful links to get you familiar with the language.
-
-[//]: # "Coach note: below is a Github style spoiler"
-
-<details>
-    <summary>Step by step guidance (up to you if you want it)</summary>
-
-    1. Create the HTML you need
-        - What elements does the form need?
-    2. Figure out what a form submit actually means / does
-    3. How to get the form data after the submit?
-    4. Calculate the amount in the desired valuta
-    5. Figure out a way to provide this data in the HTML
-
-</details>
-
-![Enjoy the drink 🍹](https://media.giphy.com/media/nDcu1RPFSz36o/giphy.gif)
+- VSCode: PHP, Markdown, CSS, HTML
+- MAMP
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <body>
     <div id="container">
-        <h1>VACATION TIME</h1>
+        <h1>VACATION TIME 🍹</h1>
         <p>So you want to go on vacation but you have to convert every amount to euro, and vice-versa.<br>
             Choose a destination and we will convert it for you!</p>
         <form class="calc-curreny" method="post">
@@ -26,63 +26,69 @@
             <input type="text" name="euro" placeholder="Amount in euro">
             <button type="submit" name="submit">Convert</button>
         </form>
+        <p id="php">
+            <?php
+            // declare(strict_types=1);
+            ini_set('display_errors', '1');
+            ini_set('display_startup_errors', '1');
+            error_reporting(E_ALL);
+            if (isset($_POST['submit'])) {
+                $amount = $_POST['convert'];
+                $euro = $_POST['euro'];
+                $destination = $_POST['size'];
+
+                echo "You are going to <b>" . $destination . "</b>!<br>";
+
+                switch ($destination) {
+                    case 'Brazil':
+                        $exchangeRate = 6.52;
+                        echo "The currency in " . $destination . " is Brazilian Real.<br>";
+                        if (empty($_POST['euro'])) {
+                            $convertedAmount = $amount * $exchangeRate;
+                            echo $amount . " Brazilian Real is " . $convertedAmount . " euro.<br>";
+                        } else {
+                            $convertedAmount = $euro * $exchangeRate;
+                            echo $euro . " euro is " . $convertedAmount .  " Brazilian Real.<br>";
+                        }
+                        break;
+                    case 'Australia':
+                        $exchangeRate = 1.58;
+                        echo "The currency in " . $destination . " is Australian Dollar.<br>";
+                        if (empty($_POST['euro'])) {
+                            $convertedAmount = $amount * $exchangeRate;
+                            echo $amount . " Australian Dollar is " . $convertedAmount . " euro.<br>";
+                        } else {
+                            $convertedAmount = $euro * $exchangeRate;
+                            echo $euro . " euro is " . $convertedAmount .  " Australian Dollar.<br>";
+                        }
+                        break;
+                    case 'Vietnam':
+                        $exchangeRate = 28356.41;
+                        echo "The currency in " . $destination . " is Vietnemese Dong.<br>";
+                        if (empty($_POST['euro'])) {
+                            $convertedAmount = $amount * $exchangeRate;
+                            echo $amount . " Vietnemese Dong is " . $convertedAmount . " euro.<br>";
+                        } else {
+                            $convertedAmount = $euro * $exchangeRate;
+                            echo $euro . " euro is " . $convertedAmount .  " Vietnamese Dong.<br>";
+                        }
+                        break;
+                    case 'Madagascar':
+                        $exchangeRate = 4673.87;
+                        echo "The currency in " . $destination . " is Malagasy ariary.<br>";
+                        if (empty($_POST['euro'])) {
+                            $convertedAmount = $amount * $exchangeRate;
+                            echo $amount . " Malagasy ariary is " . $convertedAmount . " euro.<br>";
+                        } else {
+                            $convertedAmount = $euro * $exchangeRate;
+                            echo $euro . " euro is " . $convertedAmount .  " Malagasy ariary.<br>";
+                        }
+                        break;
+                }
+            }
+            ?>
+        </p>
     </div>
-    <?php
-    if (isset($_POST['submit'])) {
-        $amount = $_POST['convert'];
-        $euro = $_POST['euro'];
-        $destination = $_POST['size'];
-
-        echo "You are going to: " . $destination . ".<br>";
-
-        switch ($destination) {
-            case 'Brazil':
-                $exchangeRate = 6.52;
-                echo "The currency in " . $destination . " is Brazilian Real.<br>";
-                if (empty($_POST['euro'])) {
-                    $convertedAmount = $amount * $exchangeRate;
-                    echo $amount . " Brazilian Real is " . $convertedAmount . " euro.<br>";
-                } else {
-                    $convertedAmount = $euro * $exchangeRate;
-                    echo $euro . " euro is " . $convertedAmount .  " Brazilian Real.<br>";
-                }
-                break;
-            case 'Australia':
-                $exchangeRate = 1.58;
-                echo "The currency in " . $destination . " is Australian Dollar.<br>";
-                if (empty($_POST['euro'])) {
-                    $convertedAmount = $amount * $exchangeRate;
-                    echo $amount . " Australian Dollar is " . $convertedAmount . " euro.<br>";
-                } else {
-                    $convertedAmount = $euro * $exchangeRate;
-                    echo $euro . " euro is " . $convertedAmount .  " Australian Dollar.<br>";
-                }
-                break;
-            case 'Vietnam':
-                $exchangeRate = 28356.41;
-                echo "The currency in " . $destination . " is Vietnemese Dong.<br>";
-                if (empty($_POST['euro'])) {
-                    $convertedAmount = $amount * $exchangeRate;
-                    echo $amount . " Vietnemese Dong is " . $convertedAmount . " euro.<br>";
-                } else {
-                    $convertedAmount = $euro * $exchangeRate;
-                    echo $euro . " euro is " . $convertedAmount .  " Vietnamese Dong.<br>";
-                }
-                break;
-            case 'Madagascar':
-                $exchangeRate = 4673.87;
-                echo "The currency in " . $destination . " is Malagasy ariary.<br>";
-                if (empty($_POST['euro'])) {
-                    $convertedAmount = $amount * $exchangeRate;
-                    echo $amount . " Malagasy ariary is " . $convertedAmount . " euro.<br>";
-                } else {
-                    $convertedAmount = $euro * $exchangeRate;
-                    echo $euro . " euro is " . $convertedAmount .  " Malagasy ariary.<br>";
-                }
-                break;
-        }
-    }
-    ?>
 </body>
 
 </html>
